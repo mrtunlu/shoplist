67a9aa07e66dce188479c93c1e276857b405510c
Mert ÜNLÜ <93090731+mrtunlu@users.noreply.github.com>
2022-06-02 16:31:59 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+        <TodoList/>
+    </div>
+  )
+}
+
+export default App
